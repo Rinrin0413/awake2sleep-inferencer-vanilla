5d8405bc9228d7ccbb7f1c89440f8d193c36ca27
Rinrin.rs <rinrin0413.valley@gmail.com>
2025-08-28 23:38:42 +0900

    🛠️ Change: update statistics and its structure

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,6 @@
 // Constants
 
-const STATISTICS = {sampleCount:392,date:'2025-07-31',stats:{awake:{mean:17.92308673478316,stdDeviation:5.956283313256553},sleep:{mean:10.056717687102038,stdDeviation:3.460559074456975},correlation:0.27352961310113827},regrModels:[{name:'4次多項式回帰',f:(x)=>5.128106740758042+1.077137940829176*x-0.09956195828094334*x**2+0.003919915914416868*x**3-0.00005065416899167597*x**4,r2:0.08431712866039276},{name:'指数回帰',f:(x)=>6.7647847710369655*Math.exp(0.01833432736163257*x),r2:0.0787984945432123},{name:'2次多項式回帰',f:(x)=>7.803418558050871+0.09180128263043144*x+0.001704700303946596*x**2,r2:0.0756192526881635},{name:'線形回帰',f:(x)=>0.15891879801001607*x+7.2084022865810375,r2:0.07481844924325232},{name:'累乗回帰',f:(x)=>4.549697877590112*x**0.2571041805844064,r2:0.07037800419877571},{name:'対数回帰',f:(x)=>2.1352071645251405*Math.log(x)+4.033410707515911,r2:0.061343550343847975}]};
+const STATISTICS = {nSamples:413,date:'2025-08-28',corr:0.29175158040509935,pVal:0.0000000015118892,variableStats:{awakeStats:{mean:18.06908797431477,median:16.91666667,stdDev:6.090319130463955,min:1.1,max:38.8,q1:14.4,q3:20.86666667},sleepStats:{mean:10.066343825690074,median:9.9,stdDev:3.448859879640087,min:2.3,max:20.11666667,q1:7.55,q3:12.46666667}},regrModels:[{name:'線形重回帰',f:(x,dow)=>{dowAdj={Friday:0.,'Monday':2.8512511457188090,'Saturday':-0.3208122449660484,'Sunday':4.4232237868070339,'Thursday':2.0778513624349153,'Tuesday':2.7544181842220610,'Wednesday':2.7766105156927017}[dow];return 0.1595523333102244*x+dowAdj+4.931501652093509},r2:0.3258639741853224,adjR2:0.3136702992997856},{name:'指数回帰',f:(x)=>6.6735643182715325*Math.exp(0.0189958038589796*x),r2:0.08818724474521666,adjR2:0.08596872222634855},{name:'4次多項式回帰',f:(x)=>5.528160014005055+0.8655926036317612*x-0.0765458982205444*(x*x)+0.0030293648215251*(x*x*x)-0.0000393249025689*(x*x*x*x),r2:0.09089029453585928,adjR2:0.08197745428621095},{name:'線形回帰',f:(x)=>0.1652147118937730*x+7.081064661830421,r2:0.08511898466887347,adjR2:0.08289299679702156},{name:'2次多項式回帰',f:(x)=>7.564737941995957+0.1113424383032895*x+0.0013473383718007*(x*x),r2:0.08564375978366245,adjR2:0.08118348544114373},{name:'3次多項式回帰',f:(x)=>7.841709111851278+0.0543277753750433*x+0.0046987187217927*(x*x)-0.0000577787701504*(x*x*x),r2:0.08574978044718029,adjR2:0.07904378861671957},{name:'累乗回帰',f:(x)=>4.3562823712151548*x**0.2721698725706173,r2:0.07869445293316124,adjR2:0.0764528335972322},{name:'対数回帰',f:(x)=>2.2742666600003565*Math.log(x)+3.6340866392458944,r2:0.07011087209818301,adjR2:0.06784836813735129}]};
 
 // Color palette for different environments.
 // Some colors were transplanted from the "Tempest Shaders for JE":
@@ -66,7 +66,7 @@ function updateResults() {
 	const awakeDuration = parseFloat(input.value);
 	const sleepDurations = STATISTICS.regrModels.reduce(
 		(acc, model) => {
-			acc[model.name] = model.f(awakeDuration);
+			acc[model.name] = model.f(awakeDuration, 'Monday'); // TODO: 曜日指定の実装
 			return acc;
 		}, {}
 	);
@@ -180,7 +180,7 @@ function updateEnv() {
 // Other
 
 // Initialize default input value, results and environment.
-input.value = STATISTICS.stats.awake.mean.toFixed(1);
+input.value = STATISTICS.variableStats.awakeStats.mean.toFixed(1);
 updateResults();
 updateEnv();
 
